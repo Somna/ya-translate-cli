da25e093b2ccae3950613e7c600751bb80033a51
sanatullov <sanatullov@trialpay.com>
2016-11-10 15:11:42 +0700

    - add pipe support - remove option -t

--- yandex-translate/cli.js
+++ yandex-translate/cli.js
@@ -12,15 +12,12 @@ else {
   version = '0.0.1';
 }
 
-
 program
-  .version(version)
-  .usage('[options] <text>')
-  .option('-l, --lang [text]', 'set lang from-to.')
-  .option('-t, --text [text]', 'text for translate.')
-  .parse(process.argv);
+    .version(version) 
+    .usage('[options] <text>')
+    .option('-l, --lang [text]', 'set lang from-to.')
+    .parse(process.argv);
 
 module.exports = {
-  text: program.args[0] || '',
   lang: program.lang || 'en-ru'
 };
--- yandex-translate/index.js
+++ yandex-translate/index.js
@@ -1,18 +1,47 @@
 var request = require('request');
+var readline = require('readline');
+
 var cli = require('./cli');
 
-var translateCallback = function(err, res, body) {
-  if (!err) {
-    console.log(JSON.parse(body).text[0]);
-  }  
+var readStdin = function(callback) {
+    var lines = [];
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      terminal: false
+    });
+    
+    rl.on('line', function(line) {
+      lines.push(line);
+    });
+    
+    rl.on('close', function() {
+      callback(lines.join('\n'));
+    });
 }
 
-var endpoint = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
-var apiKey = 'trnsl.1.1.20161110T051108Z.7ee49e46901db747.bce87285480db9e1a8d9a553b02285cc3bbffd06';
-var form = {
-  key: apiKey,
-  lang: cli.lang,
-  text: cli.text,  
-};
 
-request.post(endpoint, {form: form}, translateCallback);
+var output = function(text) {
+  var translateCallback = function(err, res, body) {
+    if (!err) {
+      console.log(JSON.parse(body).text[0]);
+    }
+  }
+  
+  var endpoint = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
+  var apiKey = 'trnsl.1.1.20161110T051108Z.7ee49e46901db747.bce87285480db9e1a8d9a553b02285cc3bbffd06';
+  var form = {
+    key: apiKey,
+    lang: cli.lang,
+    text: text,
+  };
+  request.post(endpoint, {form: form}, translateCallback);
+}
+
+
+if (!cli.text) {
+  readStdin(output);
+} 
+else {
+  output(cli.text);
+}
